6fd5c72c0624f1bf4b76f51fc61c0db718671b38
Jorge Antonio <UP200782@alumnos.upa.edu.mx>
2025-10-13 20:03:22 -0600

    Programa para hacer la prueba de las clases y de los objetos

---
+++ Clases y objetos/Clases y objetos/Models/Bebida.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;\
+
+//Aqui declaro el namespace para guardar las clases como si fueran una carpeta y despues reutilizarlas como si fueran una libreria
+
+namespace Clases_y_objetos.Models
+
+    //Aqui estoy creando la clase Bebida a la que le asigno atributos, metodos y su constructor
+{
+    internal class Bebida
+    {
+        //Estos son los atributos de la clase
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+
+        //Este es el constructor de la clase
+        public Bebida(string nombre, int cantidad) 
+        {
+            this.Nombre = nombre;
+            this.Cantidad = cantidad;
+        }
+        //Este es el metodo de la clase, en los emtodos es doonde construyes la logica de la clase
+        public void Beber(int CuantoBebio) 
+        {
+            this.Cantidad -= CuantoBebio;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//En esta parte estoy importando la clase bebida con ayuda del namespace que cree en la carpeta bebida
+using Clases_y_objetos.Models;
+namespace Clases_y_objetos
+{
+
+
+
+    //Aqui estoy creado la clase principal del programa, en la que creare un objeto de la calse bebida y usare el metodo beber que hice en la clase bebida
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Con ayuda del constructor de la clase bebida estoy creando un objeto llamado bebida1, y le doy valor a sus atributos dentro del parentesis
+            Bebida bebida1 = new Bebida("Coca cola", 100);
+            bebida1.Beber(50);  // En esta linea estoy usando el metodo beber que cree en la clase bebida, y le paso como parametro 50
+
+            //Aqui imprimo en consola la cantidad que queda de la bebida despues de haber usado el metodo beber
+            Console.WriteLine($"Bebiste 50 de {bebida1.Nombre}, ahora quedan {bebida1.Cantidad}");
+        }
+    }
+}
