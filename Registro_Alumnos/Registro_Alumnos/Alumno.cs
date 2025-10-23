0b0a4d9113a761dcc97ccd5f785c03e6aa36c0f0
Jorge Antonio <UP200782@alumnos.upa.edu.mx>
2025-10-22 22:05:31 -0600

    Primera parte del crud falso en consola

---
+++ Registro_Alumnos/Registro_Alumnos/Alumno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro_Alumnos
+{
+    public class Alumno
+    {
+        public string Nombre { get; set; }
+        public float Edad { get; set; }
+
+        public float Promedio { get; set; }
+
+        public Alumno(string nombre, float edad, float promedio)
+        {
+            this.Nombre = nombre;
+            this.Edad = edad;
+            this.Promedio = promedio;
+        }
+
+        public static Alumno agregar_alumno()
+        {
+            Console.WriteLine("Escribe el nombre del alumno que quieres agregar");
+            string Nombre = Console.ReadLine();
+            Console.WriteLine("Escribe la edad del alumno que quieres agregar");
+            float Edad = float.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe el promedio del alumno que quieres agregar");
+            float Promedio = float.Parse(Console.ReadLine());
+
+            return new Alumno(Nombre, Edad, Promedio);
+        }
+    }
+}
---
+++ Registro_Alumnos/Registro_Alumnos/Program.cs
@@ -0,0 +1,47 @@
+﻿using Registro_Alumnos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro_Alumnos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuar;
+            //Falta agregar el try catch para que no truene si el usuario ingresa algo incorrecto
+
+            List<Alumno> alumnos = new List<Alumno>();
+
+            Console.WriteLine("Por favor selecciona el numero correspondiente de la opción que deseas elegir\n");
+            Console.WriteLine("1. Agregar Alumno");
+            Console.WriteLine("2. Mostrar alumnos");
+            Console.WriteLine("3. Buscar alumno por nombre");
+            Console.WriteLine("4. Salir");
+
+            string opcion = Console.ReadLine();
+
+            switch (opcion) 
+            {
+                case "1":
+                    Alumno nuevoAlumno = Alumno.agregar_alumno();
+                    alumnos.Add(nuevoAlumno);
+                    break;
+
+                case "2":
+                    foreach (Alumno alumno in alumnos) 
+                    {
+                        Console.WriteLine($"Nombre: {alumno.Nombre}, Edad: {alumno.Edad}, Promedio: {alumno.Promedio}");
+                        //Falta que se repita el menu despues de cada accion
+                    }
+                    break;
+
+
+            }
+
+        }
+    }
+}
